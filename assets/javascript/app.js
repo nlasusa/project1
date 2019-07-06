c54c1b6a2bf05d8122b845a1e75569c14721a4ce
EvJCle <evanjcleary@gmail.com>
2019-07-06 11:51:08 -0400

    added all 3 recipes, can't get it to float right yet though

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,23 +27,6 @@ $("#button-1").on("click", function(){
 
     console.log("The calories you've chosen to search are: " + calories);
 
-    // unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories=2000&timeFrame=day")
-    // .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
-    // .header("X-RapidAPI-Key", "583e89ac2fmsh3176bf5e7b70170p19a52cjsn4591ad6ecf16")
-    // .end(function (result) {
-    //   console.log(result);
-    // });
-
-    // $.ajax({
-    //   method: 'GET',
-    //   url: 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "X-RapidAPI-Key": "583e89ac2fmsh3176bf5e7b70170p19a52cjsn4591ad6ecf16"
-    //   }
-    // })
-
-    // var question = "How much vitamin c is in 2 apples";
     $.ajax({
       url:
         "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories=" + calories + "&timeFrame=day",
@@ -65,6 +48,8 @@ $("#button-1").on("click", function(){
       console.log(recipeOne);
 
       postRecipeFunction1();
+      postRecipeFunction2();
+      postRecipeFunction3();
     });
 
 
@@ -101,20 +86,105 @@ $("#button-1").on("click", function(){
         // $(newDiv).text(ingredients[i].name);
 
         var name = ingredients[i].name;
-        var value = ingredients[i].amount.metric.value;
-        var unit = ingredients[i].amount.metric.unit;
+        var value = ingredients[i].amount.us.value.toFixed(2);
+        var unit = ingredients[i].amount.us.unit;
 
         $(newDiv).html("<hr>" + value + " " +  unit + " " + name);
 
-
-
-
-        // $(newDiv).text(ingredients[i].metric);
         $(newDiv).addClass("col-4");
 
-        console.log(ingredients[i].name);
-        // $(newDiv).text(ingredients[i].amount.metric.value);
-        // $(newDiv).text(ingredients[i].amount.metric.unit);
+        $("#recipe-container").append(newDiv);
+
+        
+      }
+      
+
+    })
+  }
+
+
+  function postRecipeFunction2(){
+
+    console.log(recipeOne);
+    console.log(recipeTwo);
+    console.log(recipeThree);
+
+    $.ajax({
+      url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + recipeTwo+ "/ingredientWidget.json",
+      type: "GET",
+      headers: {
+        "X-RapidAPI-Key":
+          "583e89ac2fmsh3176bf5e7b70170p19a52cjsn4591ad6ecf16",
+        "X-RapidAPI-Host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+      }
+    }).then(function(resp2){
+
+      console.log(resp2);
+      $("#recipe-container").show();
+      // $("#recipe-container").html(resp1);
+      
+      console.log(resp2.ingredients)
+      var ingredients = resp2.ingredients;
+
+      for (i = 0; i < ingredients.length; i++) {
+        // $("#recipe-container").text(ingredients[i].name);
+        console.log(i);
+        var newDiv = $("<div>");
+        // $(newDiv).text(ingredients[i].name);
+
+        var name = ingredients[i].name;
+        var value = ingredients[i].amount.us.value.toFixed(2);
+        var unit = ingredients[i].amount.us.unit;
+
+        $(newDiv).html("<hr>" + value + " " +  unit + " " + name);
+
+        $(newDiv).addClass("col-4");
+
+        $("#recipe-container").append(newDiv);
+
+        
+      }
+      
+
+    })
+  }
+
+
+  function postRecipeFunction3(){
+
+
+    $.ajax({
+      url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + recipeThree+ "/ingredientWidget.json",
+      type: "GET",
+      headers: {
+        "X-RapidAPI-Key":
+          "583e89ac2fmsh3176bf5e7b70170p19a52cjsn4591ad6ecf16",
+        "X-RapidAPI-Host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+      }
+    }).then(function(resp3){
+
+      console.log(resp3);
+      $("#recipe-container").show();
+      // $("#recipe-container").html(resp1);
+      
+      console.log(resp3.ingredients)
+      var ingredients = resp3.ingredients;
+
+      for (i = 0; i < ingredients.length; i++) {
+        // $("#recipe-container").text(ingredients[i].name);
+        console.log(i);
+        var newDiv = $("<div>");
+        // $(newDiv).text(ingredients[i].name);
+
+        var name = ingredients[i].name;
+        var value = ingredients[i].amount.us.value.toFixed(2);
+        var unit = ingredients[i].amount.us.unit;
+
+        $(newDiv).html("<hr>" + value + " " +  unit + " " + name);
+
+        $(newDiv).addClass("col-4");
 
         $("#recipe-container").append(newDiv);
 
